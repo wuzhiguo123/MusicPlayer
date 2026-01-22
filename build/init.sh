87d6cae004f1ae8ab0bca581f1659ba81b14eac6
wuzhiguo <345680229@qq.com>
2026-01-22 14:17:53 +0800

    socket play

--- build/init.sh
+++ build/init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-shmid=`ipcs | grep 0x6603fd03 | awk {'print $2'}`
+shmid=`ipcs | grep 0x66000002 | awk {'print $2'}`
 
 if [ ! -z $shmid ]; then
     ipcrm -m $shmid
 fi
 
 semid=`ipcs | grep 0x000003e8 | awk {'print $2'}`
-
+    
 if [ ! -z $semid ]; then
     ipcrm -s $semid
 fi
